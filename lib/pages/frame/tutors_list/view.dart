16fdd09e63f1263fd281f8a867126e965baa9357
Hung Thai <hungtnv@tenxtenx.com>
2023-10-25 22:04:51 +0700

    feat: build tutors

--- lib/pages/frame/tutors_list/view.dart
+++ lib/pages/frame/tutors_list/view.dart
@@ -10,7 +10,9 @@ class TutorsListPage extends GetView<TutorsListController> {
   @override
  
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return SingleChildScrollView( 
+
+      
       
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -18,20 +20,174 @@ class TutorsListPage extends GetView<TutorsListController> {
           Card(
             margin: EdgeInsets.all(24),
             elevation: 5, 
-            child: Column(
-              children: [
-                Text('Upcoming lession'),
-                Text('Fri, 30 Sep 22 18:30-18:55'),
-                TextButton.icon(onPressed: () {}, 
-                icon:  Image.asset('lib/images/facebook.png', width: 24, height: 24,), 
-                label: Text('Enter lesson room'))
-              ],
-              
+            child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                children: [
+                  Text(
+                    'Upcoming lession', 
+                    style: TextStyle(
+                      fontSize: 25, 
+                      fontWeight: FontWeight.bold
+                    ),
+                    
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ), 
+                  Text(
+                    'Fri, 30 Sep 22 18:30-18:55', 
+                    style: TextStyle(
+                      fontSize: 15
+                    ),
+                    ),
+
+
+                      SizedBox(
+                      height: 8,
+                    ), 
+                  TextButton.icon(onPressed: () {}, 
+                  icon:  Image.asset('lib/images/facebook.png', width: 24, height: 24,), 
+                  label: Text('Enter lesson room'))
+                ],
+                
+              ),
             ),
 
           ), 
 
-          Text("Recommended tutors")
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: Text("Recommended tutors", textAlign: TextAlign.center,),
+          ), 
+
+
+          // Card(
+          //   margin: EdgeInsets.all(24),
+          //   elevation: 5, 
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(16),
+          //     child: Column(
+          //       children: [
+          //         Row(
+          //           mainAxisSize: MainAxisSize.max,
+          //           children: [
+          //             CircleAvatar(
+          //               child: Image.asset('lib/images/facebook.png', width: 24, height: 24),
+                        
+          //             ),
+          //             SizedBox(
+          //               width: 8,
+          //             ) , 
+          //             Expanded(
+          //               child: Column(
+          //                 crossAxisAlignment: CrossAxisAlignment.start,
+          //                 children: [
+          //                   Text(
+          //                     'Keegan'
+          //                   ), 
+                                  
+          //                   Row(
+          //                     children: [
+          //                        Image.asset('lib/images/facebook.png', width: 24, height: 24,), 
+          //                        Text(
+          //                         'France'
+          //                        )
+                                  
+          //                     ],
+          //                   )
+          //                 ],
+          //               ),
+          //             ), 
+          //             Image.asset('lib/images/facebook.png', width: 24, height: 24,), 
+          //           ],
+          //         )
+          //       ],
+          //     ),
+          //   ),
+
+          // )
+
+
+          Card(
+      surfaceTintColor: Colors.white,
+      elevation: 3.0,
+      margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 16.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                InkWell(
+                  onTap: () => {},
+                  child: CircleAvatar(
+                    radius: 45,
+                    backgroundImage: AssetImage('lib/images/facebook.png'),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InkWell(
+                          onTap: () => {},
+                          child: Text(' hung thai',
+                              style: TextStyle(
+                                fontSize: 32, 
+                              )),
+                        ),
+                        Text('hung ne', style: const TextStyle(fontSize: 16)),
+                        Row(
+                          children: List<Widget>.generate(
+                            5,
+                            (index) => const Icon(Icons.star, color: Colors.amber),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                     Icons.favorite_border,
+                    color:  Colors.blue,
+                  ),
+                )
+              ],
+            ),
+            Wrap(
+              spacing: 4,
+              runSpacing: -6,
+              children: List<Widget>.generate(
+                10,
+                (index) => Chip(
+                  backgroundColor: Colors.lightBlue[50],
+                  label: Text(
+                    'haha',
+                    style: const TextStyle(fontSize: 14, color: Colors.blue),
+                  ),
+                ),
+              ),
+            ),
+            Text('thang nay day ngu vc'),
+            Align(
+              alignment: Alignment.centerRight,
+              child: OutlinedButton.icon(
+                onPressed: () => {},
+                icon: const Icon(Icons.edit_calendar),
+                label: const Text('Book'),
+              ),
+            )
+          ],
+        ),
+      ),
+    ), 
+        
           
         ],
       ),
